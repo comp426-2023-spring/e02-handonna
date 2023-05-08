f5895c3c65bfdf3e40176e67ee7e801c4d79b711
Donna Han <hdonna@email.unc.edu>
2023-05-08 04:23:21 -0400

    still need to add reset button and style

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,32 @@
-// If you would like to see some examples of similar code to make an interface interact with an API, 
-// check out the coin-server example from a previous COMP 426 semester.
-// https://github.com/jdmar3/coinserver
+document.addEventListener('DOMContentLoaded', () => {
+    const playButton = document.getElementById('play-button');
+    const gameResult = document.getElementById('game-result');
+    const resultText = document.getElementById('result-text');
+    
+    playButton.addEventListener('click', async () => {
+      const gameType = document.querySelector('input[name="game"]:checked').value;
+      const move = document.querySelector(`input[name="${gameType === 'rps' ? 'rps-move' : 'rpsls-move'}"]:checked`).value;
+      const url = `/app/${gameType}/play`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ move }),
+      });
+      const result = await response.json();
+      
+      gameResult.style.display = 'block';
+      resultText.innerHTML = `Player chose ${result.player}, Opponent chose ${result.opponent}, Result is ${result.result}.`;
+    });
+  });
+
+  
+  document.getElementById('rules-button').addEventListener('click', () => {
+      const rulesSection = document.getElementById('rules');
+      if (rulesSection.style.display === 'none') {
+          rulesSection.style.display = 'block';
+      } else {
+          rulesSection.style.display = 'none';
+      }
+  });
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//// Load most basic dependencies
+// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
 import { createRequire } from 'node:module';
@@ -20,10 +20,8 @@ if (args.debug) {
 if (args.h || args.help) {
     console.log(`
 usage: node server.js --port=5000
-
 This package serves the static HTML, CSS, and JS files in a /public directory.
 It also creates logs in a common log format (CLF) so that you can better.
-
   --stat,  -s    Specify the directory for static files to be served
                     Default: ./public/
   --port, -p    Specify the port for the HTTP server to listen on
@@ -69,36 +67,88 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
     {stream: fs.createWriteStream(path.join(logpath, 'access.log')), flags: 'a' }
 ))
 // Serve static files
-const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
-app.use('/', express.static(staticpath))
+const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public');
+app.use('/', express.static(staticpath));
+
+app.use(express.json());
+
+const determineWinner = (playerMove, opponentMove, gameType) => {
+  if (playerMove === opponentMove) {
+    return 'draw';
+  }
+
+  const winningMoves = {
+    rps: {
+      rock: 'scissors',
+      paper: 'rock',
+      scissors: 'paper',
+    },
+    rpsls: {
+      rock: ['scissors', 'lizard'],
+      paper: ['rock', 'spock'],
+      scissors: ['paper', 'lizard'],
+      lizard: ['spock', 'paper'],
+      spock: ['scissors', 'rock'],
+    },
+  };
+
+  if (winningMoves[gameType][playerMove].includes(opponentMove)) {
+    return 'win';
+  } else {
+    return 'lose';
+  }
+};
+
+const generateOpponentMove = (gameType) => {
+  const moves = gameType === 'rps' ? ['rock', 'paper', 'scissors'] : ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  return moves[Math.floor(Math.random() * moves.length)];
+};
+
+app.post('/app/:gameType/play', (req, res) => {
+  const gameType = req.params.gameType;
+  const playerMove = req.body.move;
+  const opponentMove = generateOpponentMove(gameType);
+  const gameResult = determineWinner(playerMove, opponentMove, gameType);
+
+  const result = {
+    player: playerMove,
+    opponent: opponentMove,
+    result: gameResult,
+  };
+
+  res.json(result);
+});
+
 // Create app listener
-const server = app.listen(port)
+const server = app.listen(port);
+
 // Create a log entry on start
-let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
+let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n';
 // Debug echo start log entry to STDOUT
 if (args.debug) {
-    console.info(startlog)
-} 
+  console.info(startlog);
+}
 // Log server start to file
-fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
+fs.appendFileSync(path.join(logpath, 'server.log'), startlog);
+
 // Exit gracefully and log
 process.on('SIGINT', () => {
-// Create a log entry on SIGINT
-    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
-//  Log SIGINT to file
-    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
-// Debug echo SIGINT log entry to STDOUT
+  // Create a log entry on SIGINT
+  let stoppinglog = new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n';
+  // Log SIGINT to file
+  fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog);
+  // Debug echo SIGINT log entry to STDOUT
+  if (args.debug) {
+    console.info('\n' + stoppinglog);
+  }
+  // Create a log entry on stop
+  server.close(() => {
+    let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n';
+    // Log server stop to file
+    fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog);
+    // Debug echo stop log entry to STDOUT
     if (args.debug) {
-        console.info('\n' + stoppinglog)
+      console.info('\n' + stoppedlog);
     }
-// Create a log entry on stop
-    server.close(() => {
-        let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
-// Log server stop to file
-        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
-// Debug echo stop log entry to STDOUT
-        if (args.debug) {
-            console.info('\n' + stoppedlog)
-        }    
-    })
-})
+  });
+});
